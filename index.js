18df3348bbcbf9291caf6a3059a63d28f986f4da
Abigail Chelanagt <abigailchelah@gmail.com>
2025-02-26 15:36:10 -0800

    Added my_index.js file

---
+++ index.js
@@ -0,0 +1,48 @@
+// Basic math functions
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // Increment and Decrement functions
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Parsing functions
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+  // Export functions for testing (only needed in some setups)
+  module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+    increment,
+    decrement,
+    makeInt,
+    preserveDecimal
+  };
+  
+  
